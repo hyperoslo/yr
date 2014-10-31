5a7d1fd45ecf2eb9eef09dc9efce671df7292ef0
Espen Høgbakk <espen@hogbakk.no>
2014-10-31 09:49:03 +0100

    Fix bug where Yr::Day methods would fail if period 2 had passed

--- lib/yr/models/day.rb
+++ lib/yr/models/day.rb
@@ -2,15 +2,25 @@ module Yr
   class Day
     attr_accessor :name, :description, :date, :periods
 
-    def initialize(name, description, date, periods)
+    def initialize(name=nil, description=nil, date=nil, periods=nil)
       @name        = name
       @description = description
       @date        = date
       @periods     = periods
     end
 
+    # According to the yr.no API specs, period with id 2 represents
+    # the most accurate forecast for the whole day. So if you are
+    # only going to use show one period, use that. But if that time
+    # period has passed it will not exist, so use the first available.
+    #
+    # The reason is that yr.no don't show historical data in the api.
     def day_period
-      @periods.find{|p| p.id == 2}
+      if period = @periods.find{|p| p.id == 2}
+        period
+      else
+        @periods.first
+      end
     end
 
     def temperature
