9e3edf5d4e1a5de664cb161d25f5fc88343fef78
Espen Høgbakk <espen@hogbakk.no>
2014-11-04 07:43:37 +0100

    Remove AssetUrlHelper

--- lib/yr/models/symbol.rb
+++ lib/yr/models/symbol.rb
@@ -1,17 +1,13 @@
-require 'action_view'
-
 module Yr
   class Symbol
-    include ActionView::Helpers::AssetUrlHelper
-
     attr_accessor :id, :number, :name
 
     def svg
-      asset_url("yr/symbols/svg/#{id}.svg")
+      "yr/symbols/svg/#{id}.svg"
     end
 
     def png(size: 48)
-      asset_url("yr/symbols/b#{size}/#{id}.png")
+      "yr/symbols/b#{size}/#{id}.png"
     end
   end
 end
--- spec/lib/yr/models/symbol_spec.rb
+++ spec/lib/yr/models/symbol_spec.rb
@@ -4,18 +4,18 @@ describe Yr::Symbol do
   subject { create :symbol }
   describe "#svg" do
     it "returns the url to the correct symbol" do
-      expect(subject.svg).to eq("/yr/symbols/svg/01d.svg")
+      expect(subject.svg).to eq("yr/symbols/svg/01d.svg")
     end
   end
 
   describe "#png" do
     it "returns the url to the correct symbol" do
-      expect(subject.png).to eq("/yr/symbols/b48/01d.png")
+      expect(subject.png).to eq("yr/symbols/b48/01d.png")
     end
 
     context "size is 200" do
       it "returns the url to the correct symbol" do
-        expect(subject.png(size: 200)).to eq("/yr/symbols/b200/01d.png")
+        expect(subject.png(size: 200)).to eq("yr/symbols/b200/01d.png")
       end
     end
   end
